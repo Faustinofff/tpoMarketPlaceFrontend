461fac8b6a3cea4ec07639876d4b52e875c7f0d8
Faustinofff <Faustinofiordalisi@gmail.com>
2025-10-20 17:04:33 -0300

    titulo en imagen

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -4,16 +4,22 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-r from-[#000000] via-[#0a0a20] to-[#000033] min-h-screen">
       
-      
-      <div className="relative w-full h-[500px]">
+      {/* Imagen principal un poco más abajo */}
+      <div className="relative w-full h-[500px] mt-8">
         <img
           src="/ip.png"
           alt="Imagen futurista gamer"
           className="w-full h-full object-cover"
         />
 
-        
-        <div className="absolute top-1/2 right-1/6 transform -translate-y-1/2">
+        {/* Título y frase encima de la imagen, a la izquierda */}
+        <div className="absolute top-1/4 left-12 text-left">
+          <h1 className="text-5xl font-bold text-white mb-4">ELECTROSHOP</h1>
+          <p className="text-lg text-white">Te brindamos los dispositivos originales más baratos del país</p>
+        </div>
+
+        {/* Botón movido más a la derecha */}
+        <div className="absolute top-1/2 right-[5%] transform -translate-y-1/2">
           <button
             onClick={() => window.location.href = "/productos"}
             className="bg-gradient-to-r from-[#000033] via-[#1e3fff] to-[#00ffff]
@@ -28,7 +34,6 @@ export default function Home() {
         </div>
       </div>
 
-      
       <div className="mt-10">
         <ProductList />
       </div>
