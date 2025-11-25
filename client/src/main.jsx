b6bdd95fbb079c05c869ee5e9050b1614c1acc15
ValentinBirstok <valenbirstok22@gmail.com>
2025-11-25 08:21:08 -0300

    ultimo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react';
+
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
-import {Provider} from 'react-redux'; // 👈 NUEVO
-import {store} from './redux/store'; // 👈 NUEVO
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 
 createRoot(document.getElementById('root')).render(
- <StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-</StrictMode>
+ 
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 
 );
+
